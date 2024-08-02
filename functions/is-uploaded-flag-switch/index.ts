8df4cb44348dc9439963a9466f2db5c29382b6d5
pznamir00 <pznamir00@gmail.com>
2024-08-02 13:44:03 +0200

    chore(is-uploaded-flag-switch): check package works

--- functions/is-uploaded-flag-switch/index.ts
+++ functions/is-uploaded-flag-switch/index.ts
@@ -1,4 +1,5 @@
 import { S3Handler } from "aws-lambda";
+import isTen from "is-ten";
 
 export const handler: S3Handler = async (event): Promise<void> => {
   const bucket = event.Records[0].s3.bucket.name;
@@ -6,7 +7,7 @@ export const handler: S3Handler = async (event): Promise<void> => {
 
   try {
     console.log(bucket, key);
-    console.log(event);
+    console.log(isTen(10), isTen(1000));
   } catch (error) {
     throw error;
   }
