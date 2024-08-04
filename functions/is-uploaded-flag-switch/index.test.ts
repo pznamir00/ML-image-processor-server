14139800e240f9002a89ac30f0c5df1a2d4baa43
pznamir00 <pznamir00@gmail.com>
2024-08-04 14:40:44 +0200

    fix(is-uploaded-flag-switch): fixed deps

--- functions/is-uploaded-flag-switch/index.test.ts
+++ functions/is-uploaded-flag-switch/index.test.ts
@@ -1,7 +1,28 @@
+import { Context, S3Event } from "aws-lambda";
+import { handler } from "./index";
+
 describe("isUploaded flag switch lambda", () => {
   describe("handler", () => {
-    it("test", () => {
-      expect(true).toBe(true);
+    it("passes", async () => {
+      const { event, context, callback } = setupParams();
+      await handler(event, context, callback);
     });
   });
 });
+
+function setupParams() {
+  return {
+    context: {} as Context,
+    callback: jest.fn(),
+    event: {
+      Records: [
+        {
+          s3: {
+            bucket: { name: "s3-bucket-example" },
+            object: { key: "photo.jpg" },
+          },
+        },
+      ],
+    } as S3Event,
+  };
+}
